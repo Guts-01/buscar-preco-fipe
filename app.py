71ebb1de11dd23936e2413275a4c13f83ff42838
Guto oliveira <129955858+Guts-01@users.noreply.github.com>
2025-03-23 18:44:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def index():
     return render_template("index.html", vehicle_type=vehicle_type, brands=brands, models=models, years=years, vehicle_data=vehicle_data, selected_brand=selected_brand, selected_model=selected_model, selected_year=selected_year, error_message=error_message)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
